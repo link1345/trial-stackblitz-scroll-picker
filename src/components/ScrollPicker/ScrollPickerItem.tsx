94e47cd32c0223877bf44529ed4c844db773a445
大沼 峻徳 <numa.san.1024@gmail.com>
2024-10-09 18:00:16 +0900

    タップで数字を選択できるようにした

---
+++ src/components/ScrollPicker/ScrollPickerItem.tsx
@@ -0,0 +1,57 @@
+import { ListItemText, MenuItem } from "@mui/material";
+import type { FC, ReactNode } from "react";
+
+import { SCROLL_ITEM_HEIGHT } from "./ScrollItemHeight";
+import { useJustClick } from "./hooks/useJustClick";
+
+export type ScrollPickerItemProps = {
+  /** 選択されているか */
+  selected?: boolean;
+  /** 非活性か */
+  disabled?: boolean;
+  /** 子要素 */
+  children: ReactNode;
+  /**
+   * ドラッグ操作は無効にして純粋なクリック操作のみを検知した時
+   */
+  onJustClick: () => void;
+};
+
+export const ScrollPickerItem: FC<ScrollPickerItemProps> = ({
+  selected,
+  disabled,
+  children,
+  onJustClick,
+}) => {
+  const { ref } = useJustClick({
+    disabled,
+    onJustClick: () => {
+      // ScrollPickerのスクロールの判定後にclickイベントを発火させたいのでワンサイクル遅らせる
+      setTimeout(() => {
+        onJustClick();
+      });
+    },
+  });
+
+  return (
+    <MenuItem
+      ref={ref}
+      style={{
+        height: SCROLL_ITEM_HEIGHT,
+        minHeight: "auto",
+        textAlign: "center",
+      }}
+      selected={selected}
+      disabled={disabled}
+    >
+      <ListItemText
+        sx={{
+          "& > .MuiListItemText-primary": {
+            fontWeight: selected ? "bold" : undefined,
+          },
+        }}
+        primary={children}
+      />
+    </MenuItem>
+  );
+};
